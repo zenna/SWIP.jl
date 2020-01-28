ab721b7a1f8581c4eea95c66a9ab07a2f2a972c4
Zenna Tavares <zennatavares@gmail.com>
2020-01-28 02:50:27 -0500

    rm comments

--- src/SWIP.jl
+++ src/SWIP.jl
@@ -6,12 +6,6 @@ import Cassette
 
 # export ~, ⟂, ud
 
-# Qs
-# - How should naming work?
-# - What should we take the pdf of a trace with respect to
-# - What's the interface to conditioning
-# - Syntac for potentnial outcome
-
 include("miniomega.jl")
 include("intervene.jl")
 # input("graph.jl") 
